951700ea497d9290aaee7d7fbb903fd79614231e
Priyobrato Das <priyodas12@gmail.com>
2020-08-05 01:11:23 +0530

    integer

--- src/io/javalab/strings/PrintDuplicateCharacters.java
+++ src/io/javalab/strings/PrintDuplicateCharacters.java
@@ -8,13 +8,15 @@ public class PrintDuplicateCharacters {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
         System.out.println("Enter any String::");
-        String input=scn.next();
+        String input=scn.nextLine().replaceAll("\\s","");
 
         char[] chs=input.toCharArray();
+
+        System.out.println(Arrays.toString(chs));
         //convert char to character array
         List<Character> list= Arrays.asList(input.chars().mapToObj(c -> (char)c).toArray(Character[]::new));
 
-      list.stream().filter(i->Collections.frequency(list,i)>1).collect(Collectors.toSet()).forEach(System.out::println);
+        list.stream().filter(i->Collections.frequency(list,i)>1).collect(Collectors.toSet()).forEach(System.out::println);
 
     }
 }
