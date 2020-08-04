5055d29c1e0c0adf1b89f61f61c3a150b2b04350
Priyobrato Das <priyodas12@gmail.com>
2020-08-05 00:15:14 +0530

    integer

---
+++ src/io/javalab/stream/IntToBinary.java
@@ -0,0 +1,9 @@
+package io.javalab.stream;
+
+import java.util.stream.IntStream;
+
+public class IntToBinary {
+    public static void main(String[] args) {
+        IntStream.range(1,11).mapToObj(num->Integer.toBinaryString(num)).forEach(System.out::println);
+    }
+}
