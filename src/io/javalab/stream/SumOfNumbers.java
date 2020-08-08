77ec5f4cecbd995fca4712c98780546509c3f9a4
Priyobrato Das <priyodas12@gmail.com>
2020-08-08 09:56:07 +0530

    sums

--- src/io/javalab/stream/SumOfNumbers.java
+++ src/io/javalab/stream/SumOfNumbers.java
@@ -6,11 +6,26 @@ import java.util.stream.IntStream;
 
 public class SumOfNumbers {
     public static void main(String[] args) {
+        //from a random range
         System.out.println(IntStream.range(0,10).sum());
+
+        //from a specific array
         Integer [] c=new Integer []{1,2,5,9,6,8};
 
         List<Integer> integers = Arrays.asList(c);
+
+        //way1
         System.out.println(integers.stream()
                                     .reduce(0, (a, b) -> a + b));
+        //way2
+        System.out.println(integers.stream()
+                .reduce(0,Integer::sum));
+
+        //way3
+        System.out.println(integers.stream()
+                                    .mapToInt(Integer::intValue)
+                                    .sum());
+
+
     }
 }
