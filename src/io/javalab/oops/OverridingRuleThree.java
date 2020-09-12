fd7845bc0b71f47b7f4ca991c5f6abc0eeb4c6ec
Priyobrato Das <priyodas12@gmail.com>
2020-09-12 22:28:19 +0530

    Overriding

--- src/io/javalab/oops/OverridingRuleThree.java
+++ src/io/javalab/oops/OverridingRuleThree.java
@@ -7,9 +7,19 @@ class A{
     public final void  m1(){
         System.out.println("Final implementation in Parent class!!");
     }
+
+    public void m2(){
+        System.out.println("non-final parent implementation!!");
+    }
 }
 
 class B extends A{
     //final method cant be under scope of child implementation.
+
+
+    @Override
+    public final void m2() {
+        System.out.println("final child implementation!!");
+    }
 }
 
